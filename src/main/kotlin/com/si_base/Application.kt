76e8555010b7d775c88f940cebb14fe26d62b729
ACER <loilyxtra@gmail.com>
2022-12-19 23:31:46 +0700

    init commit

---
+++ src/main/kotlin/com/si_base/Application.kt
@@ -0,0 +1,16 @@
+package com.si_base
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.si_base.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}

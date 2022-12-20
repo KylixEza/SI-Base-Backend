8b7a5426cc4e56bc5cd174c7af6a160010634fb6
ACER <loilyxtra@gmail.com>
2022-12-20 22:10:04 +0700

    forwarded routing to plugin

--- src/main/kotlin/com/si_base/plugins/Routing.kt
+++ src/main/kotlin/com/si_base/plugins/Routing.kt
@@ -1,13 +1,19 @@
 package com.si_base.plugins
 
+import com.si_base.route.SIBaseRoute
 import io.ktor.server.routing.*
 import io.ktor.http.*
-import io.ktor.locations.*
+import io.ktor.server.locations.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
+import org.koin.ktor.ext.inject
 
+@KtorExperimentalLocationsAPI
 fun Application.configureRouting() {
+
+    val route by inject<SIBaseRoute>()
+
     install(Locations) {
     }
 
@@ -15,26 +21,6 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
-        get<MyLocation> {
-            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
-        }
-        // Register nested routes
-        get<Type.Edit> {
-            call.respondText("Inside $it")
-        }
-        get<Type.List> {
-            call.respondText("Inside $it")
-        }
+        route.apply { this@routing.initRoutes() }
     }
-}
-
-@Location("/location/{name}")
-class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
-@Location("/type/{name}")
-data class Type(val name: String) {
-    @Location("/edit")
-    data class Edit(val type: Type)
-
-    @Location("/list/{page}")
-    data class List(val type: Type, val page: Int)
 }

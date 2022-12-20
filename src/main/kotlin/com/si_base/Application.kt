386bd88be8492370ec444df27714ebaa0d2fbc63
ACER <loilyxtra@gmail.com>
2022-12-20 22:10:21 +0700

    start on local

--- src/main/kotlin/com/si_base/Application.kt
+++ src/main/kotlin/com/si_base/Application.kt
@@ -1,16 +1,28 @@
 package com.si_base
 
+import com.si_base.di.databaseModule
+import com.si_base.di.repositoryModule
+import com.si_base.di.routeModule
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.si_base.plugins.*
+import io.ktor.server.locations.*
+import org.koin.core.logger.Level
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = System.getenv("PORT").toInt(), host = "localhost", module = Application::module)
         .start(wait = true)
 }
 
+@OptIn(KtorExperimentalLocationsAPI::class)
 fun Application.module() {
+    install(Koin) {
+        slf4jLogger(Level.ERROR)
+        modules(listOf(databaseModule, repositoryModule, routeModule))
+    }
     configureSerialization()
     configureRouting()
 }

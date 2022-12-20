4d54c07f2964f79503cb483b443bd04134d44a51
ACER <loilyxtra@gmail.com>
2022-12-20 22:09:17 +0700

    create mapper

---
+++ src/main/kotlin/com/si_base/util/Mapper.kt
@@ -0,0 +1,12 @@
+package com.si_base.util
+
+import com.si_base.data.table.StudentTable
+import com.si_base.model.student.StudentResponse
+import org.jetbrains.exposed.sql.ResultRow
+
+fun ResultRow.toStudentResponse() = StudentResponse(
+    studentId = this[StudentTable.studentId],
+    name = this[StudentTable.name],
+    origin = this[StudentTable.origin],
+    datePlaceBirth = this[StudentTable.datePlaceBirth]
+)
